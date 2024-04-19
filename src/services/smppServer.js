4d45412b297dc7cfe459e781094f1d5e3c7d76d1
Husain-Zreik <husain.zreik1@gmail.com>
2024-04-19 13:20:43 +0300

    edit the deliver method

--- src/services/smppServer.js
+++ src/services/smppServer.js
@@ -29,18 +29,16 @@ export default function startSMPPServer() {
 				const messageID = generateMessageID();
 				const destinationAddr = pdu.destination_addr;
 				const messageContent = pdu.short_message.message;
+				const currentTime = format(new Date(), 'YYMMDDHHmm');
 
-				if (pdu.sm_default_msg_id === 1) {
-					session.send(pdu.response());
-				} else {
-					session.send(pdu.response({ message_id: messageID }));
-				}
+				session.send(pdu.response({ message_id: messageID }));
+
 				// const buf = Buffer.from(`id:${messageID} sub:001 dlvrd:001 submit date:${format(new Date(), 'YYMMDDHHmm')} done date:${format(new Date(), 'YYMMDDHHmm')} stat:DELIVRD err:000 text:`);
+				const deliveryReceiptMessage = `id:${messageID} sub:001 dlvrd:001 submit date:${currentTime} done date:${currentTime} stat:DELIVRD err:000 text: ${messageContent}`;
 
-				// var deliver_sm = {
+				// const deliver_sm = {
 				// 	service_type: '',
 				// 	source_addr_ton: 0,
-				// 	source_addr_npi: 0,
 				// 	source_addr: destinationAddr,
 				// 	dest_addr_ton: 0,
 				// 	dest_addr_npi: 0,
@@ -54,28 +52,21 @@ export default function startSMPPServer() {
 				// 	replace_if_present_flag: 0,
 				// 	data_coding: 0,
 				// 	sm_default_msg_id: 0,
-				// 	short_message: {
-				// 		// udh: new Uint8Array(buf),
-				// 		// message: new Uint8Array(buf),
-				// 		message: `id:${messageID} sub:001 dlvrd:001 submit date:${format(new Date(), 'YYMMDDHHmm')} done date:${format(new Date(), 'YYMMDDHHmm')} stat:DELIVRD err:000 text:`,
-				// 	},
 				// 	message_state: 2,
 				// 	receipted_message_id: messageID,
+				// 	short_message: {
+				// 		// udh: new Uint8Array(buf),
+				// 		message: deliveryReceiptMessage,
+				// 	},
 				// };
-				// var pdu = new smpp.PDU('deliver_sm', deliver_sm);
-				// session.send(pdu);
-				// console.log(pdu);
 
-				const currentTime = format(new Date(), 'YYMMDDHHmm');
+				// const hi = new smpp.PDU('deliver_sm', deliver_sm);
+				// session.send(hi);
+				// console.log(hi);
 
-				// Construct the delivery receipt message string
-				const deliveryReceiptMessage = `id:${messageID} sub:001 dlvrd:001 submit date:${currentTime} done date:${currentTime} stat:DELIVRD err:000 text:`;
-
-				// Create the deliver_sm object with the formatted short_message
-				const deliver_sm = {
+				session.deliver_sm({
 					service_type: '',
 					source_addr_ton: 0,
-					source_addr_npi: 0,
 					source_addr: destinationAddr,
 					dest_addr_ton: 0,
 					dest_addr_npi: 0,
@@ -89,63 +80,21 @@ export default function startSMPPServer() {
 					replace_if_present_flag: 0,
 					data_coding: 0,
 					sm_default_msg_id: 0,
-					short_message: {
-						message: deliveryReceiptMessage,
-					},
 					message_state: 2,
 					receipted_message_id: messageID,
-				};
+					short_message: {
+						// udh: new Uint8Array(buf),
+						message: deliveryReceiptMessage,
+					},
+				}, function (deliverPdu) {
+					if (deliverPdu.command_status !== 255) {
+						console.log(`Successful Message ID for ${message.number}:`, deliverPdu.message_id);
+					} else {
+						console.error(`Error sending SMS to ${message.number}:`, deliverPdu.command_status);
+					}
+				});
 
-				// Create and send the PDU
-				const hi = new smpp.PDU('deliver_sm', deliver_sm);
-				session.send(hi);
-				console.log(hi);
-
-
-
-				// var deliver_sm = {
-				// 	service_type: '',
-				// 	source_addr_ton: 0,
-				// 	source_addr_npi: 0,
-				// 	source_addr: destinationAddr,
-				// 	dest_addr_ton: 0,
-				// 	dest_addr_npi: 0,
-				// 	destination_addr: '',
-				// 	esm_class: 4,
-				// 	protocol_id: 0,
-				// 	priority_flag: 0,
-				// 	schedule_delivery_time: '',
-				// 	validity_period: '',
-				// 	registered_delivery: 0,
-				// 	replace_if_present_flag: 0,
-				// 	data_coding: 0,
-				// 	sm_default_msg_id: 0,
-				// 	short_message: {
-				// 		message_id: messageID + "second delivery",
-				// 		message: messageContent
-				// 	}
-				// };
-				// var pdu = new smpp.PDU('deliver_sm', deliver_sm);
-				// session.send(pdu);
-
-				// session.deliver_sm({
-				// 	source_addr: destinationAddr,
-				// 	short_message: messageContent,
-				// 	esm_class: 4,
-				// }, function (deliverPdu) {
-				// 	if (deliverPdu.command_status != 255) {
-				// 		console.log(`Successful Message ID for ${destinationAddr}:`, deliverPdu.message_id);
-				// 		// updateDeliveredRecord(deliverPdu.message_id);
-				// 	}
-				// });
 			});
-
-			// session.send(new smpp.PDU('query_sm_resp', {
-			// 	message_id: messageID,
-			// 	final_date: '20240418',
-			// 	message_state: 2,
-			// 	error_code: 0,
-			// }));
 
 			session.on('unbind', function (pdu) {
 				session.send(pdu.response());
@@ -176,18 +125,15 @@ function generateMessageID() {
 function updateDeliveredRecord(messageId) {
 	if (!connection || connection.state === 'disconnected') {
 		console.error('Database connection is not available or disconnected.');
-		// Re-establish the database connection
 		connection.connect((err) => {
 			if (err) {
 				console.error('Error reconnecting to the database:', err);
 				return;
 			}
 			console.log('Reconnected to the database.');
-			// After reconnection, execute the query
 			executeUpdateQuery(messageId);
 		});
 	} else {
-		// If the connection is available, execute the query directly
 		executeUpdateQuery(messageId);
 	}
 }
