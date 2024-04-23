512578be5235e98aaf9877ed30a103a4aa644a72
Husain-Zreik <husain.zreik1@gmail.com>
2024-04-23 09:23:51 +0300

    create close smppserver function

--- src/services/smppServer.js
+++ src/services/smppServer.js
@@ -1,23 +1,28 @@
-import connection from '../../config/dbConnection.js';
 import smpp from 'smpp';
 import fecha from 'fecha';
 const { format } = fecha;
 const { createServer } = smpp;
 
-export default function startSMPPServer() {
+let server;
+
+export default function startSMPPServer(clientIp, clientPort, clientId, clientPassword) {
 	var server = createServer({
 		debug: true,
 		rejectUnauthorized: false,
 	}, function (session) {
-		session.on('error', function (err) {
-			console.error('An error occurred:', err);
-		});
+		console.log(session);
+
+		if (session.socket.remoteAddress !== clientIp || session.socket.remotePort !== clientPort) {
+			console.error('Invalid client IP address or port');
+			session.close();
+			return;
+		}
 
 		session.on('bind_transceiver', function (pdu) {
 			session.pause();
 			console.log("Received bind_transceiver request:", pdu);
 
-			if ((pdu.system_id == 'alaac' && pdu.password == 'alaac') || (pdu.system_id == 'alaav' && pdu.password == 'alaav')) {
+			if (pdu.system_id == clientId && pdu.password == clientPassword) {
 				session.send(pdu.response());
 				session.resume();
 			} else {
@@ -35,34 +40,6 @@ export default function startSMPPServer() {
 
 				// const buf = Buffer.from(`id:${messageID} sub:001 dlvrd:001 submit date:${format(new Date(), 'YYMMDDHHmm')} done date:${format(new Date(), 'YYMMDDHHmm')} stat:DELIVRD err:000 text:`);
 				const deliveryReceiptMessage = `id:${messageID} sub:001 dlvrd:001 submit date:${currentTime} done date:${currentTime} stat:DELIVRD err:000 text: ${messageContent}`;
-
-				// const deliver_sm = {
-				// 	service_type: '',
-				// 	source_addr_ton: 0,
-				// 	source_addr: destinationAddr,
-				// 	dest_addr_ton: 0,
-				// 	dest_addr_npi: 0,
-				// 	destination_addr: '',
-				// 	esm_class: 4,
-				// 	protocol_id: 0,
-				// 	priority_flag: 0,
-				// 	schedule_delivery_time: '',
-				// 	validity_period: '',
-				// 	registered_delivery: 0,
-				// 	replace_if_present_flag: 0,
-				// 	data_coding: 0,
-				// 	sm_default_msg_id: 0,
-				// 	message_state: 2,
-				// 	receipted_message_id: messageID,
-				// 	short_message: {
-				// 		// udh: new Uint8Array(buf),
-				// 		message: deliveryReceiptMessage,
-				// 	},
-				// };
-
-				// const hi = new smpp.PDU('deliver_sm', deliver_sm);
-				// session.send(hi);
-				// console.log(hi);
 
 				session.deliver_sm({
 					service_type: '',
@@ -96,6 +73,10 @@ export default function startSMPPServer() {
 
 			});
 
+			session.on('error', function (err) {
+				console.error('An error occurred:', err);
+			});
+
 			session.on('unbind', function (pdu) {
 				session.send(pdu.response());
 				session.close();
@@ -107,8 +88,8 @@ export default function startSMPPServer() {
 		});
 	});
 
-	server.listen(2775, function () {
-		console.log('SMPP server listening on port 2775');
+	server.listen(clientPort, function () {
+		console.log(`SMPP server listening on port ${clientPort}`);
 	});
 }
 
@@ -121,34 +102,14 @@ function generateMessageID() {
 	return `${timestamp}${counter.toString().padStart(3, '0')}`;
 }
 
-
-function updateDeliveredRecord(messageId) {
-	if (!connection || connection.state === 'disconnected') {
-		console.error('Database connection is not available or disconnected.');
-		connection.connect((err) => {
-			if (err) {
-				console.error('Error reconnecting to the database:', err);
-				return;
-			}
-			console.log('Reconnected to the database.');
-			executeUpdateQuery(messageId);
+export function stopSMPPServer() {
+	if (server) {
+		server.close(() => {
+			console.log('SMPP server stopped');
 		});
 	} else {
-		executeUpdateQuery(messageId);
+		console.warn('SMPP server is not running');
 	}
 }
-
-function executeUpdateQuery(messageId) {
-	const updateQuery = `UPDATE sent_to SET is_delivered = 1 WHERE id = ?`;
-
-	connection.query(updateQuery, [messageId], (error, results) => {
-		if (error) {
-			console.error(`Error updating SentTo record with ID ${messageId}:`, error);
-		} else {
-			console.log(`SentTo record with ID ${messageId} delivered successfully.`);
-		}
-	});
-}
-
 
 startSMPPServer();
