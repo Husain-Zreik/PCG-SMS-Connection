df4a2459f53d800ad1dee7e0e0f469cab56a6700
Husain-Zreik <husain.zreik1@gmail.com>
2024-04-30 11:00:38 +0300

    edit the command status

--- src/services/smppServer.js
+++ src/services/smppServer.js
@@ -143,11 +143,14 @@ export default function startSMPPServer() {
 						console.log('it is a test message and invalid');
 						messageID = generateMessageID(true);
 						messageID = '';
+						session.send(pdu.response({
+							message_id: messageID,
+							command_status: 12,
+						}));
 					} else {
 
 						session.send(pdu.response({
 							message_id: messageID,
-							command_status: 12,
 						}));
 					}
 					// const deliveryReceiptMessage = `id:${messageID} sub:001 dlvrd:001 submit date:${currentTime} done date:${currentTime} stat:DELIVRD err:000 text: ${messageContent}`;
